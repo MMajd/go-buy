6f2b36157f5dfb0f018860f2a51df869072f9075
mmajd <mabulmagd55@gmail.com>
2022-08-16 16:16:03 +0200

    init commit

---
+++ src/main/java/com/mmajd/gobuy/GoBuyRootApp.java
@@ -0,0 +1,11 @@
+package com.mmajd.gobuy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoBuyRootApp {
+    public static void main(String[] args) {
+        SpringApplication.run(GoBuyRootApp.class, args);
+    }
+}
---
+++ web/src/main/java/com/mmajd/gobuy/web/GoBuyApplication.java
@@ -0,0 +1,11 @@
+package com.mmajd.gobuy.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoBuyApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GoBuyApplication.class, args);
+    }
+}
